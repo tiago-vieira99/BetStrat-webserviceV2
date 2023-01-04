af874b3fd247beaa2e56629c41d420855446274b
Tiago Vieira <tiago.vieira@farfetch.com>
2023-01-04 19:31:49 +0000

    change url constant

--- javascript/constants.js
+++ javascript/constants.js
@@ -1,6 +1,6 @@
  //var API_URL = window.location.host.substring(0, window.location.host.indexOf(':'))+":8880";
-// const API_URL= "54.172.210.130:8880";
-const API_URL= "localhost:8080";
+const API_URL= "54.172.210.130:8880";
+//const API_URL= "localhost:8080";
 const EH_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRSMl0iAf9AFkdJIYrz2rAwqzumP9U3cMwNLT6LdqkiEn2A94SL2jtdLmyN59h59DcG57FHftYqFDgS/pubhtml";
 const OD_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTU3oegceHoFnXIXMIdgk16r0uIdaLaLo0fHRTUHunhTN2AxJdLZ2n__Q58Sam5EIaRhe_gsW0YBdua/pubhtml";
 const DH_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRAniDOaLgquFk3YZ7YR3dFZCzH6fe4UQZgq924SJocXha7U3ShpBU1SWz4Modr_ecr62e4KOHLuKZb/pubhtml";
