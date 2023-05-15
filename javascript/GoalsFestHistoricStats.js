85b579cff9e94fb359150504ddfbdd9f2c543c10
Tiago Vieira <tiago.vieira@farfetch.com>
2023-05-15 16:54:16 +0100

    add missing files in last commit

---
+++ javascript/GoalsFestHistoricStats.js
@@ -0,0 +1,25 @@
+var urlArgs = location.search.substring(1).split('&');
+var teamName = decodeURIComponent(urlArgs[1]);
+$('.teamNameTitle').append("<b>" + teamName + " :: Historic Goals Fest Stats</b><br>");
+
+callGetGoalsFestHistoricDataByTeam(teamName);
+
+var teams;
+
+
+function addDataToTable(statsData) {
+    $(document).ready(function() {
+      $('#goalsFestDataByTeamTable').append(
+        '<tr>' +
+        '<td style="padding-top: 0; padding-bottom: 0;" class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell"><a href="' + statsData.url + '" style="color: black"><b><u>' + statsData.season + '</u></b></a></td>' +
+        '<td style="padding-top: 0; padding-bottom: 0;" class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + statsData.competition + '</td>' +
+        '<td style="padding-top: 0; padding-bottom: 0;" class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + statsData.goalsFestRate + ' %</td>' +
+        '<td style="padding-top: 0; padding-bottom: 0;" class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + statsData.numGoalsFest + ' / ' + statsData.numMatches + '</td>' +
+        '<td style="padding-top: 0; padding-bottom: 0;" class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + statsData.stdDeviation + '</td>' +
+        '<td style="padding-top: 0; padding-bottom: 0;" class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + statsData.coefDeviation + '</td>' +
+        '<td style="padding-top: 0; padding-bottom: 0;" class="u-border-1 u-border-grey-40 u-border-no-left u-border-no-right u-table-cell">' + statsData.noGoalsFestSequence + '</td></tr>'
+      );
+    });
+}
+
+
---
+++ javascript/MarginWins.js
@@ -0,0 +1,86 @@
+var x, y;
+var context = document.querySelector('#graph').getContext('2d');
+var chart;
+
+callGetSeqEvolution(EURO_HANDICAP_PATH);
+callGetSeqInfo(EURO_HANDICAP_PATH);
+
+function syncData() {
+  var url = "http://" + API_URL + "/api/betstrat/sync";
+
+  fetch(url, {
+      method: 'POST'
+    });
+    setTimeout(() => { window.location.reload(); }, 2000);
+}
+
+function chartSetup(days, profit) {
+  var limitedDaysArray = days.slice(-250);
+  var limitedProfitArray = profit.slice(-250);
+  var data = {
+    type: 'line',
+    data: {
+      labels: limitedDaysArray,
+      datasets: [{
+        backgroundColor: "rgba(39, 245, 191, 0.2)",
+        strokeColor: "red",
+        pointColor: "red",
+        pointStrokeColor: "red",
+        pointHighlightFill: "red",
+        pointHighlightStroke: "red",
+        data: limitedProfitArray,
+        fill: true,
+        tension: 0.3,
+      borderColor: 'rgba(52,155,69, 1)',
+      borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        filler: {
+          propagate: false,
+        },
+        legend: {
+          position: 'top',
+          display: false
+        },
+        title: {
+          display: true,
+          text: 'Strategy Profit Evolution',
+          font: {
+            size: 26
+          }
+        }
+      },
+      scales: {
+        y: {
+          stacked: true,
+          title: {
+            display: true,
+            text: 'Profit (€)',
+            font: {
+              size: 16
+            }
+          }
+        }
+      }
+    },
+  };
+  return data;
+}
+
+// ---------------------------------------------------------------------------------------------------------
+
+document.getElementById("seasonSelect").addEventListener("change",function() {
+  const season = this.value;
+  if (season == "all") {
+    chart.destroy();
+    callGetSeqInfo(EURO_HANDICAP_PATH);
+    callGetSeqEvolution(EURO_HANDICAP_PATH);
+  } else {
+    chart.destroy();
+    callGetSeqEvolutionBySeason(EURO_HANDICAP_PATH, season);
+  }
+})
+
