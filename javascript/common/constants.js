b165e2cb749f5f0b84e2d8c5e2a762c824a13375
Tiago Vieira <vie0002t@belgium.fhm.de>
2025-09-05 15:12:32 +0100

    change host

--- javascript/common/constants.js
+++ javascript/common/constants.js
@@ -1,8 +1,8 @@
  //var API_URL = window.location.host.substring(0, window.location.host.indexOf(':'))+":8880";
-//const API_URL= "http://core-app:8880";
-const API_URL= "208.85.23.20:8880";
- const DATA_STATS_API_URL= "208.85.23.20:8090";
- //const DATA_STATS_API_URL= "localhost:8090";
+const API_URL= "http://158.220.112.147:8880";
+//const API_URL= "localhost:8880";
+const DATA_STATS_API_URL= "158.220.112.147:8090";
+//const DATA_STATS_API_URL= "localhost:8090";
 const EH_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRSMl0iAf9AFkdJIYrz2rAwqzumP9U3cMwNLT6LdqkiEn2A94SL2jtdLmyN59h59DcG57FHftYqFDgS/pubhtml";
 const OD_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTU3oegceHoFnXIXMIdgk16r0uIdaLaLo0fHRTUHunhTN2AxJdLZ2n__Q58Sam5EIaRhe_gsW0YBdua/pubhtml";
 const DH_DATA_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRAniDOaLgquFk3YZ7YR3dFZCzH6fe4UQZgq924SJocXha7U3ShpBU1SWz4Modr_ecr62e4KOHLuKZb/pubhtml";
